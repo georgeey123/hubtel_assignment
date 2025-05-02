7e0606c96d4fbd086780d17b2c1d4130c1161628
George <georgeey123@gmail.com>
2025-05-02 20:32:15 +0000

    feat: update base entity

--- domain/Models/BaseEntity.cs
+++ domain/Models/BaseEntity.cs
@@ -3,4 +3,6 @@ namespace domain;
 public class BaseEntity
 {
     public int Id { get; set; }
+    public DateTime CreatedAt { get; set; }
+    public DateTime UpdatedAt { get; set; }
 }
--- tests/obj/Debug/net8.0/tests.AssemblyInfo.cs
+++ tests/obj/Debug/net8.0/tests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c0a67af2e01169a1aa2a40a9381af27dc7f62dc7")]
 [assembly: System.Reflection.AssemblyProductAttribute("tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
